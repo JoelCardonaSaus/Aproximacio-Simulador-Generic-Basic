3bb54d278ad39e2c1f7bfe68c3a1d6a8bc48fae6
Joel Cardona Saus <joel.cardona@estudiantat.upc.edu>
2023-06-06 00:05:29 +0200

    Afegeix el test de la sortida

--- Assets/Testing/Tests/SortidaTests.cs
+++ Assets/Testing/Tests/SortidaTests.cs
@@ -6,19 +6,33 @@ using UnityEngine.TestTools;
 
 public class SortidaTests
 {
-    public GameObject entitatTemporal;
-    /*
+    private GameObject motor;
+    private GameObject sortida;
+    private GameObject entitatTemporalPrefab = Resources.Load<GameObject>("EntitatsTemporals/PaquetPrefab");
+
+    [SetUp]
+    public void setup(){
+        motor = GameObject.Instantiate(Resources.Load("MotorSimulador/MotorDeSimulacio")) as GameObject;
+        sortida = GameObject.Instantiate(Resources.Load("LlibreriaObjectes/Sortida/Sortida")) as GameObject;
+        sortida.transform.parent = motor.transform;
+    }
+
+    [TearDown]
+    public void teardown(){
+        MotorSimuladorScript.Instancia.ReiniciarSimulador();
+        Object.Destroy(sortida.gameObject);
+        Object.Destroy(motor.gameObject);
+    }
+
     [Test]
     public void SortidaDestrueixUnaEntitat()
     {
-        GameObject sortida = GameObject.Instantiate(Resources.Load("LlibreriaObjectes/Sortida/Sortida")) as GameObject;
-        SortidaScript sortidaScript = sortida.GetComponent<SortidaScript>();
-        entitatTemporal = GameObject.CreatePrimitive(PrimitiveType.Plane);
-        
-        sortidaScript.inicialitzaPerFerTests();
-        //sortidaScript.recieveObject(entitatTemporal);
+        sortida.GetComponent<SortidaScript>().IniciaSimulacio();
+        if (sortida.GetComponent<SortidaScript>().EstaDisponible(null)){
+            GameObject entitatAux = GameObject.Instantiate(entitatTemporalPrefab);
+            sortida.GetComponent<SortidaScript>().RepEntitat(entitatAux, null);
+        }
 
-        Assert.That(sortidaScript.getNEntitatsDestruides(), Is.EqualTo(1));
+        Assert.That(sortida.GetComponent<SortidaScript>().getNEntitatsDestruides(), Is.EqualTo(1));
     }
-    */
 }
